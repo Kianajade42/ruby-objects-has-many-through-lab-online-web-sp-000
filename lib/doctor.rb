8ded2499af5a8bb9ffdd073a13fc9d4731864532
Kiana Long <kianajade42.kl@gmail.com>
2021-02-07 01:05:36 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,10 +8,6 @@ class Doctor
     @@all << self
   end
 
-  def self.all
-    @@all
-  end
-
   def new_appointment(date,patient)
     Appointment.new(date,patient,self)
   end
@@ -23,4 +19,7 @@ class Doctor
   def patients
     appointments.map { |appointments|appointments.patient }
   end
+end
+def self.all
+  @@all
 end
